4ee1b1de1a707e00a8468cfa170381c98fee5ee9
Garnett Puett <gpuett@gmail.com>
2018-05-02 18:14:47 -0700

    Add new task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule }  from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent
   ],
   imports: [
     BrowserModule,
